a9cce47d636633cdfd50456056db4ee8b85a7e6f
Yu Okamoto <yuokamoto1988@gmail.com>
2023-12-30 13:25:51 +0900

    add CharacterMap

--- Source/rclUEExamples/rclUEExamples.Build.cs
+++ Source/rclUEExamples/rclUEExamples.Build.cs
@@ -1,9 +1,25 @@
 // Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
 
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class rclUEExamples : ModuleRules
 {
+    private void CreateSymbolicLinkToEnginePack(string InName)
+    {
+        string ProjectPath = Path.GetDirectoryName(Target.ProjectFile.FullName);
+        string EngineContentTemplatePath = Path.Combine(EngineDirectory, "../Templates/TemplateResources/High");
+
+        string targetPath = Path.Combine(EngineDirectory, "../Templates/TemplateResources/High",  InName, "Content");
+        string path = Path.Combine(ProjectPath, "Content", InName);
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateSymbolicLink(path, targetPath);
+            System.Console.WriteLine("Create symbolic link to Engine contents: " + targetPath + " -> " + path );
+        }
+    }
+
     public rclUEExamples(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -12,5 +28,8 @@ public class rclUEExamples : ModuleRules
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "rclUE", "RapyutaSimulationPlugins", "ROSAssets" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        CreateSymbolicLinkToEnginePack("LevelPrototyping");
+        CreateSymbolicLinkToEnginePack("Characters");
     }
 }
